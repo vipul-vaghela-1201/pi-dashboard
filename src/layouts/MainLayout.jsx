d77e779e355b1f6688d6c7160cd405cedfbe625c
vipul-vaghela-1201 <info.vipul1201@gmail.com>
2025-06-27 20:07:27 +0530

    foldername import issue

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Box } from "@mui/material";
-import TopBar from "../components/TopBar";
+import TopBar from "../Components/TopBar";
 
 const MainLayout = () => {
   return (
