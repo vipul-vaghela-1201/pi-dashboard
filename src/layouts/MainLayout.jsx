380361d3b2d48373309727407bdcfc150e442ba3
vipul-vaghela-1201 <info.vipul1201@gmail.com>
2025-06-27 20:04:44 +0530

    initial commit

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import TopBar from "../components/TopBar";
 import { Box } from "@mui/material";
+import TopBar from "../components/TopBar";
 
 const MainLayout = () => {
   return (
